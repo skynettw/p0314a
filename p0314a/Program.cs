c9345c87df966f581608091635b097312dc1da8e
skynettw <skynet.tw@gmail.com>
2023-03-14 11:35:06 +0800

    finish the program

--- p0314a/Program.cs
+++ p0314a/Program.cs
@@ -10,7 +10,14 @@ namespace p0314a
     {
         static void Main(string[] args)
         {
+            int a, b;
+            Console.Write("a=");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("b=");
+            b = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine($"{a}+{b}=" + (a + b).ToString());
+            Console.ReadLine();
         }
     }
 }
