eab09351c7a2a41b23efc9e443ed24964a8dd96b
skynettw <skynet.tw@gmail.com>
2023-03-14 12:03:44 +0800

    final version

--- p0314a/Program.cs
+++ p0314a/Program.cs
@@ -10,6 +10,7 @@ namespace p0314a
     {
         static void Main(string[] args)
         {
+            /*
             int a, b;
             Console.Write("a=");
             a = Convert.ToInt32(Console.ReadLine());
@@ -17,6 +18,46 @@ namespace p0314a
             b = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine($"{a}+{b}=" + (a + b).ToString());
+            
+            */
+            /*
+            string resultMsg = "";
+            int score = 0;
+
+            Console.Write("Your score=");
+            score = Convert.ToInt32(Console.ReadLine());
+
+            
+            if (score >= 60)
+            {
+                resultMsg = "Pass";
+            } else
+            {
+                resultMsg = "Fail";
+            }
+            */
+
+            /*
+
+            resultMsg = (score >= 60) ? "Pass" : "Fail";
+
+            Console.WriteLine("Your result is " + resultMsg);
+            */
+            // 0314作業要求如下：
+            // 把下面這個程式改為 do/while迴圈，
+            // 然後可以計算最大值、最小值、平均數、個數、標準差
+            // 附上執行結果（20個數）的擷圖（.png or .jpg）
+            // 把專案打包，加上上面的擷圖檔案全部壓縮成zip檔案上傳到教學平台
+            int score=0, total=0;
+            Console.Write("score=");
+            score = Convert.ToInt32(Console.ReadLine());
+            while (score>=0)
+            {
+               total += score;
+               Console.Write("score=");
+               score = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Total = " + total.ToString());
             Console.ReadLine();
         }
     }
